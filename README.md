74a346b8bca041bbe08641cfb229cc7eb25f061d
Nanashi <npj.kuijstermans@student.avans.nl>
2020-12-15 13:22:19 +0100

    add readme and rename API swagger displayname

---
+++ README.md
@@ -0,0 +1,9 @@
+# lotus-server
+A RESTful backend api for the LOTUS 2021 project
+
+## Requirements
+* .NET 5.0 (or later)
+
+## Supported Editors
+* Visual Studio 2019 (v16.8 and later)
+* JetBrains Rider (v2020.3 and later)
--- RestApi/Startup.cs
+++ RestApi/Startup.cs
@@ -27,7 +27,7 @@ namespace RestApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "RestApi", Version = "v1"}); });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "LOTUS RESTapi", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +37,7 @@ namespace RestApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LOTUS RESTapi v1"));
             }
 
             app.UseHttpsRedirection();
