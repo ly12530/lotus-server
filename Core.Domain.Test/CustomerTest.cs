bff73c906834d93a07c21bec3bebbfb060b4ce63
marci <mv.ngasiman@student.avans.nl>
2021-01-13 14:44:38 +0100

    Added some tests to User and Customer

---
+++ Core.Domain.Test/CustomerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Core.Domain.Test
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void Fails_If_Customer_EmailAddress_Is_Not_Valid_Format()
+        {
+            // Arrange
+            var emailAddress = "JorisWessels_omega.lol,dasda";
+
+            var thrownException = false;
+
+            // Act
+            try
+            {
+                var regexResult = Regex.Match(emailAddress, "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*").Success;
+
+                if (!regexResult) throw new InvalidOperationException();
+            }
+            catch
+            {
+                thrownException = true;
+            }
+
+            // Assert
+            Assert.True(thrownException);
+        }
+    }
+}
